d4bc614ffc4a2b20faffcdd5bbb72a00e16c9ae7
Ganesh Acharya <31121986g@gmail.com>
2019-12-27 14:47:26 +0530

    adding controllers

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const guest = require('./controllers/guest.controller')
+const users = require('./controllers/users.controller')
 
-router.use('/', guest.home);
+router.get('/', guest.home);
+router.post('/register', users.register);
 
 module.exports = router;
