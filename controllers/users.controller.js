5a3239e975ec38cb6349b913549c415103580ea0
Ganesh Acharya <31121986g@gmail.com>
2020-01-01 14:43:46 +0530

    Adding models to application

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,8 +1,11 @@
 
+const User = require('../models/user.model')
 exports.home = function(request, response) {
     response.render('home-guest');
 }
 
 exports.register = function(request, response) {
+    let user = new User(request.body);
+    user.register();
     response.send('Thank you for registering with us');
 }
