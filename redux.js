58a5bd0e2c9c40f32b0c31b5cd79e5f9135730d5
khazadoom <31121986g@gmail.com>
2020-02-08 08:25:57 +0530

    redux next steps

--- redux.js
+++ redux.js
@@ -1,30 +1,3 @@
-// function reducer(state={counter: 0}, action) {
-//     switch (action.type) {
-//         case 'INCREMENT_COUNTER':
-            
-//             return {
-//                 counter: state.counter + 1,
-//                 ...state
-//             }
-        
-//             case 'DECREMENT_COUNTER':
-        
-//             return {
-//                 counter: state.counter - 1,
-//                 ...state
-//             }
-    
-//         default:
-//             return {
-//                 ...state
-//             }
-//     }
-
-    
-// }
-
-// expect()
-
 
 function reducer(state=0, action) {
     switch (action.type) {
